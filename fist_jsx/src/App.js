6373ae8fd78cdb087673fbd6e3a0fedac394138a
o_sam <ollie.samuel11@gmail.com>
2021-11-01 03:50:53 -0400

    Changed the display to 'Hello Dojo'

--- fist_jsx/src/App.js
+++ fist_jsx/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <h1>Hello Dojo!</h1>
       </header>
+
       <body>
         <h2>Things I need to do:</h2>
         <ul>
@@ -15,6 +17,7 @@ function App() {
           <li>Feed the dogs</li>
         </ul>
       </body>
+      
     </div>
   );
 }
